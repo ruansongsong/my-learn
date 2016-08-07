33c06956d73495519f0fa180587cc6700fe68d6c
ruansongsong <ruansongsong@gmail.com>
2016-08-07 11:37:01 +0800

    learnt some thing

---
+++ es6-learn/async.js
@@ -0,0 +1,14 @@
+/**
+* @Author: ruansongsong <ruansongsong>
+* @Date:   2016-08-06T15:43:13+08:00
+* @Email:  ruansongsong@gmail.com
+* @Last modified by:   ruansongsong
+* @Last modified time: 2016-08-06T15:44:47+08:00
+*/
+/**
+ * 异步编程
+ * 1.回调函数
+ * 2.事件监听
+ * 3.发布/订阅模式
+ * 4.Promise
+ */
